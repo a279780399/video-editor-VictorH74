dc51cc4c1e0ec0f67bcc1a72dbe39bc8a39ea901
VictorH74 <victorh.almeida7@gmail.com>
2024-02-16 19:07:10 -0300

    fix: DownloadFinalVideo span animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,15 +6,13 @@ const config: Config = {
     extend: {
       keyframes: {
         shake: {
-          'from': { rotate: "0%" },
-          'to': { rotate: '-10%' },
-          // '0%, 100%': { rotate: "0%" },
-          // '10%, 30%, 50%, 70%, 90%': { rotate: '-10%' },
-          // '20%, 40%, 60%, 80%': { rotate: '10%' },
+          "0%, 40%, 100%": { rotate: "0deg" },
+          "50%, 70%, 90%": { rotate: "10deg" },
+          "60%, 80%": { rotate: "-10deg" },
         }
       },
       animation: {
-        shake: 'shake 0.5s ease-in-out infinite',
+        shake: 'shake 3s ease infinite',
       }
     },
   },
